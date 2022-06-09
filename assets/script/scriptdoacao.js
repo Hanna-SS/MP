7772595097327f1764b65fbccfdcffdfb2be2e3a
Hanna_SS <aninha.souza123@gmail.com>
2022-06-09 15:43:50 -0300

    responsividade

---
+++ assets/script/scriptdoacao.js
@@ -0,0 +1,12 @@
+let cart = [];
+
+const c = (el) =>document.querySelector(el); //limpa codigo da função de dar um item
+const cs = (el) =>document.querySelectorAll(el); //limpa o codigo da função dar uma lista/array
+
+
+epicvipJson.map(() => {
+    
+
+
+
+})
---
+++ assets/script/vips.js
@@ -0,0 +1,11 @@
+let epicvipJson = [
+    {id:1, name:'Madeira', img:'assets/images/madeira.png', price:20, ptp:['100h'], description:'Nesse VIP não existe uma bonificação de itens.'},
+    {id:2, name:'Bronze', img:'assets/images/bronze.png', price:30, ptp:['150h'], description:'01 Replicador + 1,000 Elementos + 02 Therezinos'},
+    {id:3, name:'Prata', img:'assets/images/prata.png', price:40, ptp:['200h'], description:'01 Replicador + 1,500 Elementos + 02 Therezinos + 02 Yutirannus'},
+    {id:4, name:'Ouro', img:'assets/images/ouro.png', price:50, ptp:['250h'], description:'01 Replicador + 2,000 Elementos + 02 Therezinos + 02 Yutirannus + 02 Rex'},
+    {id:5, name:'Diamante', img:'assets/images/diamante.png', price:100, ptp:['600h'], description:'01 Replicador + 4,500 Elementos + 04 Casais de dinos (spawnados pelo adm)'},
+    {id:6, name:'Platina', img:'assets/images/platina.png', price:200, ptp:['1.300h'], description:'02 Replicador + 10,000 Elementos + 02 Geradores Tek + 08 Casais de dinos (spawnados pelo adm)'},
+    {id:7, name:'Topazio', img:'assets/images/topazio.png', price:300, ptp:['1.300h'], description:'03 Replicador + 16,000 Elementos + 03 Geradores Tek + 01 Tek Hover + 01 conjunto Tek Alta Qualidade + 12 Casais de dinos (spawnados pelo adm)'},
+    {id:8, name:'Rubi', img:'assets/images/rubi.png', price:400, ptp:['1.300h'], description:'04 Replicador + 22,000 Elementos + 04 Geradores Tek + 01 Tek Hover + 02 conjunto Tek Alta Qualidade + 16 Casais de dinos (spawnados pelo adm)'},
+    {id:9, name:'Safira', img:'assets/images/safira.png', price:500, ptp:['1.300h'], description:'05 Replicador + 30,000 Elementos + 05 Geradores Tek + 01 Tek Hover + 05 conjunto Tek Alta Qualidade + 20 Casais de dinos (spawnados pelo adm)'}
+];
